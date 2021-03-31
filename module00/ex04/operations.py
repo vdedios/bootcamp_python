b28e49a9364077f6c1e7cdfc87013df794607fa0
Victor <victor.dedios@outlook.com>
2021-03-31 22:09:54 +0200

    improve ex04 style

--- module00/ex04/operations.py
+++ module00/ex04/operations.py
@@ -3,22 +3,23 @@
 import sys
 
 def usage(err):
-    print(err + 'Usage: python operations.py <number1> <number2>\n'
+    if err: print(f'Input error: {err}')
+    print('Usage: python operations.py <number1> <number2>\n'
            'Example:\n'
             '   python operations.py 10 3')
 
-def check_input_error(args):
+def check_input(args):
     if len(args) == 1:
         usage('')
         return False
     if len(args) > 3:
-        usage('too many arguments')
+        usage('too many arguments\n')
         return False
     if len(args) < 3:
-        usage('missing argument')
+        usage('missing argument\n')
         return False
     if not (args[1].isdecimal() and args[2].isdecimal()):
-        usage('only numbers')
+        usage('only numbers\n')
         return False
     return True
 
@@ -34,7 +35,7 @@ def calc(a, b):
         print(f'Remainder:   {a % b}')
 
 def main():
-    check_input_error(sys.argv) and calc(int(sys.argv[1]), int(sys.argv[2]))
+    check_input(sys.argv) and calc(int(sys.argv[1]), int(sys.argv[2]))
 
 if __name__ == "__main__":
     main()
